ea5dc74be3c4415589e49c2abe454a892ccb1986
mpriessner <43908762+mpriessner@users.noreply.github.com>
2021-01-04 16:29:07 +0000

    save_h5py function

--- load_functions/reconstruct_image.py
+++ load_functions/reconstruct_image.py
@@ -55,3 +55,42 @@ def save_image(temp_img, folder_option, slice_count, file_count, save_location_i
   # io.imsave(f"/{save_location}/{name}.tif",img)
   io.imsave(save_location_image+f"/{file_name}_file_{file_count}_%02d.tif"%slice_count, img)
   
+ 
+
+def save_as_h5py(img_list, permutation_list, zt_list, file_nr, multiplyer, product_image_shape):
+    '''this function saves the the single images of each 4D file into one h5py file'''
+    zt_dim = len(zt_list)
+    xy_dim = int(product_image_shape/multiplyer)
+    h5py_safe_location_list = []
+    # saving all the images in the xyz dimension in a h5py file
+    for image in img_list:
+      h5py_safe_location = f"/content/ZoomInterpolation/results/{image}.hdf5"
+      h5py_safe_location_list.append(h5py_safe_location)
+      with h5py.File(h5py_safe_location, 'w') as f:
+        for permutation in permutation_list:
+          for zt in tqdm(zt_list):
+            temp_img_3D = np.zeros((len(file_nr), multiplyer*xy_dim, multiplyer*xy_dim))
+            for single_file_nr, single_file in enumerate(file_nr):
+              temp_img_2D = np.zeros((multiplyer*xy_dim, multiplyer*xy_dim))
+
+              counter_x = 0
+              counter_y = 0
+              for num, fraction in enumerate(fraction_list):
+                if counter_x == multiplyer:
+                  counter_x = 0
+                  counter_y+=1
+                key = f"{image}_{fraction}_{permutation}_{zt}/{single_file}"
+                # print(key)
+                img_path = os.path.join(interpolate_location, key)
+                img = AICSImage(img_path)
+                img = img.get_image_data("YX", Z=0, C=0, S=0, T=0)
+                img = img.astype('uint8')
+                temp_img_2D[counter_x*xy_dim:(counter_x+1)*xy_dim,counter_y*xy_dim:(counter_y+1)*xy_dim] = img
+                counter_x += 1
+              temp_img_3D[single_file_nr,:,:] = temp_img_2D
+            name = f"{image}_{permutation}_{zt}"
+            f.create_dataset(f"{name}", data=np.array(temp_img_3D, dtype=np.uint8))
+
+    return h5py_safe_location_list
+
+
