3d3a744eb5ee09b30be4aed56f397c9647066382
mpriessner <43908762+mpriessner@users.noreply.github.com>
2021-01-04 17:43:41 +0000

    Update reconstruct_image.py

--- load_functions/reconstruct_image.py
+++ load_functions/reconstruct_image.py
@@ -9,6 +9,9 @@ from aicsimageio import AICSImage, imread
 from aicsimageio.transforms import reshape_data
 from aicsimageio.writers import png_writer 
 import numpy as np
+import h5py
+
+
 def get_file_list(folder_path):
   """This function takes a folder_path and returns a list of files sorted"""
   # get a list of files in the folder
@@ -50,14 +53,15 @@ def save_image(temp_img, folder_option, slice_count, file_count, save_location_i
   img = img.get_image_data("CSTZYX")
   if folder_option == "upsample-z" or folder_option == "downsample-z":
     img= reshape_data(img, "CSTZYX","SCTZYX")
+    io.imsave(save_location_image+f"/{file_name}_Z.tif", img)
+
   elif folder_option == "upsample-t" or folder_option == "downsample-t":
     img= reshape_data(img, "CSTZYX","SZTCYX")
+    io.imsave(save_location_image+f"/{file_name}_T.tif", img)
   # io.imsave(f"/{save_location}/{name}.tif",img)
-  io.imsave(save_location_image+f"/{file_name}_file_{file_count}_%02d.tif"%slice_count, img)
-  
- 
 
-def save_as_h5py(img_list, permutation_list, zt_list, file_nr, multiplyer, product_image_shape):
+
+def save_as_h5py(img_list, permutation_list, fraction_list, zt_list, file_nr, interpolate_location, multiplyer, product_image_shape):
     '''this function saves the the single images of each 4D file into one h5py file'''
     zt_dim = len(zt_list)
     xy_dim = int(product_image_shape/multiplyer)
@@ -72,7 +76,6 @@ def save_as_h5py(img_list, permutation_list, zt_list, file_nr, multiplyer, produ
             temp_img_3D = np.zeros((len(file_nr), multiplyer*xy_dim, multiplyer*xy_dim))
             for single_file_nr, single_file in enumerate(file_nr):
               temp_img_2D = np.zeros((multiplyer*xy_dim, multiplyer*xy_dim))
-
               counter_x = 0
               counter_y = 0
               for num, fraction in enumerate(fraction_list):
@@ -92,5 +95,3 @@ def save_as_h5py(img_list, permutation_list, zt_list, file_nr, multiplyer, produ
             f.create_dataset(f"{name}", data=np.array(temp_img_3D, dtype=np.uint8))
 
     return h5py_safe_location_list
-
-
