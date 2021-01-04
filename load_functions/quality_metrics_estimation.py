614b6dee16c246d10794cffbe97a21a9da2a3b52
mpriessner <43908762+mpriessner@users.noreply.github.com>
2021-01-04 15:27:34 +0000

    Create quality_metrics_estimation.py

---
+++ load_functions/quality_metrics_estimation.py
@@ -0,0 +1,71 @@
+from skimage.metrics import structural_similarity
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage import img_as_float32
+import numpy as np
+
+
+def normalize(x, pmin=3, pmax=99.8, axis=None, clip=False, eps=1e-20, dtype=np.float32):
+    """This function is adapted from Martin Weigert"""
+    """Percentile-based image normalization."""
+
+    mi = np.percentile(x,pmin,axis=axis,keepdims=True)
+    ma = np.percentile(x,pmax,axis=axis,keepdims=True)
+    return normalize_mi_ma(x, mi, ma, clip=clip, eps=eps, dtype=dtype)
+
+
+def normalize_mi_ma(x, mi, ma, clip=False, eps=1e-20, dtype=np.float32):#dtype=np.float32
+    """This function is adapted from Martin Weigert"""
+    if dtype is not None:
+        x   = x.astype(dtype,copy=False)
+        mi  = dtype(mi) if np.isscalar(mi) else mi.astype(dtype,copy=False)
+        ma  = dtype(ma) if np.isscalar(ma) else ma.astype(dtype,copy=False)
+        eps = dtype(eps)
+
+    try:
+        import numexpr
+        x = numexpr.evaluate("(x - mi) / ( ma - mi + eps )")
+    except ImportError:
+        x =                   (x - mi) / ( ma - mi + eps )
+
+    if clip:
+        x = np.clip(x,0,1)
+
+    return x
+
+def norm_minmse(gt, x, normalize_gt=True):
+    """This function is adapted from Martin Weigert"""
+
+    """
+    normalizes and affinely scales an image pair such that the MSE is minimized  
+     
+    Parameters
+    ----------
+    gt: ndarray
+        the ground truth image      
+    x: ndarray
+        the image that will be affinely scaled 
+    normalize_gt: bool
+        set to True of gt image should be normalized (default)
+    Returns
+    -------
+    gt_scaled, x_scaled 
+    """
+    if normalize_gt:
+        gt = normalize(gt, 0.1, 99.9, clip=False).astype(np.float32, copy = False)
+    x = x.astype(np.float32, copy=False) - np.mean(x)
+    #x = x - np.mean(x)
+    gt = gt.astype(np.float32, copy=False) - np.mean(gt)
+    #gt = gt - np.mean(gt)
+    scale = np.cov(x.flatten(), gt.flatten())[0, 1] / np.var(x.flatten())
+    return gt, scale * x
+
+
+#--------------------------------------------------------------
+def get_full_file_paths(folder):
+  import os
+  list_files = []
+  for root, dirs, files in os.walk(folder):
+      for file in files:
+          file_path = os.path.join(root, file)
+          list_files.append(file_path)
+  return list_files
