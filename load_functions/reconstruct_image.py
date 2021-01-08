d34a5675b85c9645c833c8d7a79733046e395240
mpriessner <43908762+mpriessner@users.noreply.github.com>
2021-01-08 12:13:55 +0000

    us io library instead of aicsImage

--- load_functions/reconstruct_image.py
+++ load_functions/reconstruct_image.py
@@ -27,29 +27,24 @@ def get_folder_list(source_path):
   return folder_list
 
 
+
 def save_image(temp_img, folder_option, slice_count, file_count, save_location_image, file_name):
   """ This function saves the temp image and re-structures the channels in the right order for the z-dimension"""
-  
+  # remove the first slice of zeros
   temp_img_final = temp_img[1:,:,:,:]
-  # if folder_option == "upsample-z":
-  #   temp_img_final = np.swapaxes(temp_img_final, 0, 1)
-  io.imsave("/content/temp.tif",temp_img_final)
-  img = AICSImage("/content/temp.tif")
-  img = img.get_image_data("CSTZYX")
-  
   if folder_option == "upsample-z" or folder_option == "downsample-z":
-    img= reshape_data(img, "CSTZYX","SCTZYX")
-    io.imsave(save_location_image+f"/{file_name}_Z.tif", img)
+    io.imsave(save_location_image+f"/{file_name}_Z.tif", temp_img_final)
 
   elif folder_option == "upsample-t" or folder_option == "downsample-t" :
-    img= reshape_data(img, "CSTZYX","SZTCYX")
-    io.imsave(save_location_image+f"/{file_name}_T.tif", img)
+    temp_img_final = np.swapaxes(temp_img_final, 0, 1)
+    print(temp_img_final.shape)
+    temp_img_final = temp_img_final[:-1,:,:,:] # remove the last image to get the same dimensions
+    io.imsave(save_location_image+f"/{file_name}_T.tif", temp_img_final)
 
   elif folder_option == "zoom":
-    img= reshape_data(img, "CSTZYX","SZTCYX")
-    io.imsave(save_location_image+f"/{file_name}_zoom.tif", img)
+    temp_img_final = np.swapaxes(temp_img_final, 0, 1)
+    io.imsave(save_location_image+f"/{file_name}_zoom.tif", temp_img_final)
     
-
 def save_as_h5py(img_list, permutation_list, fraction_list, zt_list, file_nr, interpolate_location, multiplyer, product_image_shape):
     '''this function saves the the single images of each 4D file into one h5py file'''
     zt_dim = len(zt_list)
@@ -74,15 +69,15 @@ def save_as_h5py(img_list, permutation_list, fraction_list, zt_list, file_nr, in
                   counter_x = 0
                   counter_y+=1
                 key = f"{image}_{fraction}_{permutation}_{zt}/{single_file}"
-                # print(key)
                 img_path = os.path.join(interpolate_location, key)
-                img = AICSImage(img_path)
-                img = img.get_image_data("YX", Z=0, C=0, S=0, T=0)
+                img = io.imread(img_path)
+                img = img[:,:,0] # otherwise there are 3 channels
                 img = img.astype('uint8')
                 temp_img_2D[counter_x*xy_dim:(counter_x+1)*xy_dim,counter_y*xy_dim:(counter_y+1)*xy_dim] = img
                 counter_x += 1
               temp_img_3D[single_file_nr,:,:] = temp_img_2D
             name = f"{image}_{permutation}_{zt}"
             f.create_dataset(f"{name}", data=np.array(temp_img_3D, dtype=np.uint8))
+            print(temp_img_3D.shape)
 
     return h5py_safe_location_list
